9101d6a6acb322e648a6170c155b48b4cbaa9012
Karel Minarik <karmi@karmi.cz>
2009-03-30 17:57:39 +0200

    Added rudimentary PDF formatter (HTMLDOC-based)
    Use like this:
    
        $ markout -f pdf path/to/my/file.txt
    
    NOTE: It would be *great* to figure out how to use Prawn here

---
+++ lib/markout/formatters/pdf/pdf.rb
@@ -0,0 +1,27 @@
+require 'tempfile'
+require File.join(File.dirname(__FILE__), '..', 'html', 'html.rb')
+
+# Depends on HTMLDOC utility (http://www.easysw.com/htmldoc/)
+module Markout
+
+  class Pdf < Formatter
+
+    def export
+      `cat "#{tempfile.path}" | /opt/local/bin/htmldoc -t pdf \
+          --bodyfont "Helvetica" --headfootfont "Helvetica" \
+          --no-compression --color --embedfonts \
+          --header "" --footer .1. --links --no-title \
+          --toctitle "" --tocheader "..." --tocfooter "..." \
+          -`
+    end
+
+    private
+
+    def tempfile
+      tempfile = Tempfile.new(File.basename(@document.path) + '.html')
+      tempfile << Markout::Html.new(@document).export
+    end
+
+  end
+
+end
