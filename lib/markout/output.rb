2f3bc26d55c0bdf524f739ad6c8698f4ff6230bb
Karel Minarik <karmi@karmi.cz>
2009-03-30 17:27:41 +0200

    Add Markout::Output factory

---
+++ lib/markout/output.rb
@@ -0,0 +1,49 @@
+module Markout
+
+  class Output
+
+    attr_reader :document, :format, :formatter
+
+    def initialize(path, options = {})
+      @path      = path
+      @document  = Document.new(@path)
+      @options   = options
+      @format    = @options[:format]   || 'html'
+      @formatter = pick_formatter.new( @document, :template => @options[:template] || 'default' )
+      @output    = ''
+    end
+
+    def export(to_file=false)
+      @output = @formatter.export
+      write_file if to_file
+      return @output
+    end
+
+    def output_path
+      if @options[:output]
+        File.join( File.expand_path( @options[:output] ), @formatter.filename )
+      else
+        @document.base_path.join(@formatter.filename)
+      end
+    end
+
+    private
+
+    def write_file
+      File.open(output_path, 'w') { |f| f.write @output }
+    end
+
+    def pick_formatter
+      unless Markout::const_defined?(@format.to_s.capitalize)
+        begin
+          require File.join( File.dirname(__FILE__), 'formatters', @format, @format  ) 
+        rescue Exception => e
+          raise LoadError, "Formatter '#{@format}' not found! (#{e.message})"
+        end
+      end
+      Markout::const_get(@format.to_s.capitalize)
+    end
+
+  end
+
+end
---
+++ test/markout_output_test.rb
@@ -0,0 +1,29 @@
+require File.join(File.dirname(__FILE__), 'test_helper')
+require 'markout/output'
+
+module Markout
+  class OutputTest < Test::Unit::TestCase
+
+    def test_default_initialization
+      assert_nothing_raised { default_output }
+      assert_not_nil @output.document
+      assert_not_nil @output.format
+      assert_not_nil @output.formatter
+      assert_equal Markout::Html, @output.formatter.class
+    end
+
+    def test_export
+      default_output
+      assert_equal fixture('markdown.html'), @output.export
+    end
+
+    def test_export_to_file
+      # TODO
+    end
+
+    def default_output
+      @output = Output.new( fixture_file('markdown.txt') )
+    end
+
+  end
+end
